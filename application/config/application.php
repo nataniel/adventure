a9e55d4832b8674ce3a1b05c23a900cd6f63163d
nataniel <artur@jedlinski.pl>
2020-04-11 19:17:42 +0200

    login with Steam, Google

--- application/config/application.php
+++ application/config/application.php
@@ -17,6 +17,21 @@ return [
     'facebook' => [
         'app_id' => '536959080539510',
         'app_secret' => '9e094cca94677bdcc677623b39e86af2',
-        'default_graph_version' => 'v4.0',
+        'default_graph_version' => 'v6.0',
+    ],
+
+    /**
+     * http://steamcommunity.com/dev/apikey
+     */
+    'steam' => [
+        'api_key' => '6F4E2F5112CDE286D5C83D359C781510',
+    ],
+
+    /**
+     * https://console.developers.google.com/apis/credentials?project=silicon-cocoa-255607
+     */
+    'google' => [
+        'client_id' => '708272906850-ah71j2864oge9bfan3fve2nfih9hntea.apps.googleusercontent.com',
+        'client_secret' => 'L160OtK1axHPEUiKFmtAak_F',
     ],
 ];
